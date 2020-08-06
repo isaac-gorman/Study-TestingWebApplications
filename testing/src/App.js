ee156004ec11bc5204e47df0d89d7f524c6d4ade
Isaac Gorman <emailme@isaacg.net>
2020-08-06 10:38:14 -0500

    Importance of testing questions outlined, and AutomatedTesting.js component routed. Up next => Route to React-testing-library and outline question

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { Route, Link } from "react-router-dom";
 import "./App.css";
+import AutomatedTesting from "./📔notes/AutomatedTesting";
 
 function App() {
   return (
@@ -10,32 +11,55 @@ function App() {
           <h5>Automated Unit Test</h5>
         </header>
       </div>
-      <div>
-        <section>
-          <h3>Module Overview:</h3>
-          <h4>Learning Objective</h4>
-          <p>
-            In this module I will learn about automated testing, and how to
-            write unit test, with{" "}
-            <span style={{ fontWeight: "bold" }}>Jest</span> . Jest is a React
-            testing-library
-          </p>
-        </section>
-        <h4>Learning Outcomes:</h4>
-        <ul>
-          <li>
-            I should be able to define what automated testing is and its
-            importance.
-          </li>
-          <li>
-            I will be demonstrate the ability to write a test on react
-            components using "react-testing-library"
-          </li>
-          <li>
-            Use the <span style={{ fontWeight: "bold" }}>"testFire()"</span>{" "}
-            event to test user interactions from the "react-testing-library"
-          </li>
-        </ul>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "80vwh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Route exact path="/">
+          <div>
+            <section>
+              <h3>Module Overview:</h3>
+              <h4>Learning Objective</h4>
+              <p>
+                In this module I will learn about automated testing, and how to
+                write unit test, with{" "}
+                <span style={{ fontWeight: "bold" }}>Jest</span> . Jest is a
+                React testing-library
+              </p>
+            </section>
+            <h4>Learning Outcomes:</h4>
+            <ul>
+              <li>
+                I should be able to define what automated testing is and its
+                importance.
+              </li>
+              <Link to="/note1">
+                <button>Learn More</button>
+              </Link>
+              <br />
+              <br />
+              <li>
+                I will be demonstrate the ability to write a test on react
+                components using "react-testing-library"
+              </li>
+              <li>
+                I will be able to use the{" "}
+                <span style={{ fontWeight: "bold" }}>"testFire()"</span> event
+                to test user interactions from the "react-testing-library"
+              </li>
+            </ul>
+          </div>
+        </Route>
+
+        <Route exact path="/note1">
+          <AutomatedTesting />
+        </Route>
       </div>
     </div>
   );
---
+++ testing/src/📔notes/AutomatedTesting.js
@@ -0,0 +1,86 @@
+import React from "react";
+
+class AutomatedTesting extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Automated Testing</h3>
+        <h4>Learning Objective:</h4>
+        <p>
+          Explain the importance of automated testing throughout the development
+          process
+        </p>
+        <div>
+          <h5>Q: Why is testing essential for software quality control?</h5>
+          <br />
+          <h3>Why testing is important</h3>
+          <h5>Q: What does testing minimize?</h5>
+          <br />
+
+          <h5>
+            Q: List some benifits when testing is conducted regualry and
+            correctly:
+          </h5>
+          <br />
+
+          <h5>
+            Q: List some negative consequnces of not testing regualry and
+            correctly:
+          </h5>
+          <br />
+
+          <h5>
+            Q: List some negative consequnces of not testing regualry and
+            correctly:
+          </h5>
+          <br />
+
+          <h3>The various types of test</h3>
+          <h5>
+            Q: List some of the classifactions for testing a software engineer
+            may come across:
+          </h5>
+          <br />
+
+          <h5>
+            Q: List some of the classifactions for testing a software engineer
+            may come across:
+          </h5>
+          <br />
+          <h5>
+            Q: What is the level of testing in which several units of software
+            are tested as a group?
+          </h5>
+          <br />
+          <h5>
+            Q: What is the type of testing in which the enitre application is
+            tested form end to end?
+          </h5>
+          <br />
+          <h5>
+            Q: What is the type of testing in which I would isolate smaller
+            units of the test?
+          </h5>
+          <br />
+
+          <h3>When testing is conducted</h3>
+          <h5>
+            Q: What are some things to consider when deciding whether or not to
+            manualy test, or automate testing?
+          </h5>
+          <br />
+          <h5>Q: When is it appropirate not to create an automated test?</h5>
+          <br />
+
+          <h3>How to conduct test</h3>
+          <h5>Q: What is a common framework for testing?</h5>
+          <br />
+          <h5>Q: In development how might this framework be implimented?</h5>
+          <br />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AutomatedTesting;
